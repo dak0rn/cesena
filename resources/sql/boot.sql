f1bfd2282d48cd24b4b23e0f64db05e8528fcd72
Daniel Koch <daniel@suitsoft.eu>
2016-09-26 13:18:44 +0200

    Fix remapping and ignore ^$

--- resources/sql/boot.sql
+++ resources/sql/boot.sql
@@ -1,3 +1,3 @@
 -- :name check-system-table
 -- :doc "Checks if the taobaibai system table exists"
-select count(name) as "exists" from sqlite_master where name = 'taobaibai';
+select count(name) as "exists" from sqlite_master where name = 'cesana';
