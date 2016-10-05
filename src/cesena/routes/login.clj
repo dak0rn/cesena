4321fdeda0c9b1645362253c007d3224000ce9ea
Daniel Koch <daniel@suitsoft.eu>
2016-10-05 14:54:47 +0200

    Find the user when logging in

--- src/cesena/routes/login.clj
+++ src/cesena/routes/login.clj
@@ -1,6 +1,7 @@
 ;; login.clj - Login route
 (ns cesena.routes.login
   (:require [ compojure.core :refer [ GET POST ] ]
+            [ cesena.services.user :refer [ find-user ] ]
             [ cesena.views.login :refer [ render-login ] ]))
 
 ;;; Route handler functions
@@ -13,7 +14,24 @@
   [ ]
   (render-login))
 
+(defn-
+  ^{
+    :doc "POST handler that tries to perform a login"
+    :added "1.0.0"
+  }
+  do-login-handler
+  [ request ]
+  (let [ params (:params request)
+         name (:username params)
+         pass (:password params)
+         user (find-user name pass) ]
+    (println "User " user)
+    (render-login true)))
+
+
+
 ;; Exported routes
 (def routes [
   (GET "/login" [ ] (login-handler))
+  (POST "/login" request (do-login-handler request))
 ])
