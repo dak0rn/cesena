86f9bd50a3e2ebbd98471a1b2a6efac626339bc6
Daniel Koch <daniel@suitsoft.eu>
2016-10-06 06:47:40 +0200

    Create a session when the login is successful

--- src/cesena/routes/login.clj
+++ src/cesena/routes/login.clj
@@ -2,6 +2,8 @@
 (ns cesena.routes.login
   (:require [ compojure.core :refer [ GET POST ] ]
             [ cesena.services.user :refer [ find-user ] ]
+            [ cesena.services.security :refer [ create-jwt ] ]
+            [ cesena.config :refer [ config ] ]
             [ cesena.views.login :refer [ render-login ] ]))
 
 ;;; Route handler functions
@@ -25,8 +27,25 @@
          name (:username params)
          pass (:password params)
          user (find-user name pass) ]
-    (println "User " user)
-    (render-login true)))
+    (if-not user
+      ;; If no user has been found render the login with
+      ;; the error message.
+      (render-login true)
+
+      ;; Otherwise the cookie is created
+      (let [ uid (:user_id user)
+             jwt-field (get-in [ :security :jwt :field ] config)
+             jwt (create-jwt { :jti uid }) ]
+        ;; Response map
+        {
+          ;; Redirect to the root
+          :status 302 :headers { "Location" "/" }
+
+          ;; Set the cookie
+          :cookies (assoc {} jwt-field jwt)
+
+          :body ""
+       }))))
 
 
 
