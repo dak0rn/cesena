30e7f4556136e1e4e2684c8bd40da9b12f5d455d
Daniel Koch <daniel@suitsoft.eu>
2016-10-09 12:19:08 +0200

    Optimize the mobile view for the administration

--- src/cesena/views/partials/document.clj
+++ src/cesena/views/partials/document.clj
@@ -13,7 +13,7 @@
     [ :html
       [ :head
         [ :title (h title) ]
-        [ :meta { :name "viewport" :content "width=device-width, initial=scale=1.0" } ]
+        [ :meta { :name "viewport" :content "width=device-width, initial-scale=1.0" } ]
         [ :link { :rel "stylesheet" :href "/cesena.css" } ]
       ]
       [ :body children ]
