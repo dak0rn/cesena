7b9c99fe5acbe784a6b653eb1af94e103a784907
Daniel Koch <daniel@suitsoft.eu>
2016-10-17 20:38:00 +0200

    Add the required service for the book updating

--- resources/sql/library.sql
+++ resources/sql/library.sql
@@ -9,3 +9,10 @@ SELECT * FROM cesena_book WHERE title LIKE :term;
 -- :name query-specific-book :? :1
 -- :doc "Select a specific book identified by the given id"
 SELECT * FROM cesena_book WHERE book_id = :bid;
+
+-- :name query-update-book :!
+-- :doc "Updates a book"
+UPDATE cesena_book
+    SET title = :title
+        date = :date
+    WHERE book_id = :book_id;
--- src/cesena/services/library.clj
+++ src/cesena/services/library.clj
@@ -1,6 +1,9 @@
 ;; library.clj - Library services
 (ns cesena.services.library
-  (:require [ cesena.database :refer [ db make-queries ] ]))
+  (:require [ cesena.database :refer [ db make-queries ] ]
+            [ cesena.config :refer [ config ] ]
+            [ clojure.java.io :refer [ file ] ]
+            [ clj-time.core :refer [ now ] ]))
 
 (make-queries "sql/library.sql")
 
@@ -29,3 +32,22 @@
   [ book-id ]
   (let [ param { :bid book-id } ]
     (query-specific-book db param)))
+
+(defn
+  update-book
+  "Updates the given book in the database by saving it.
+   Only the title can be changed. The change timestamp
+   will be set to the current time"
+  { :added "0.1.0" }
+  [ book ]
+  (let [ updated-book (assoc book :date (str (now))) ]
+    (query-update-book db updated-book)
+    updated-book))
+
+(defn
+  folder-locked?
+  "Determines if the application has write permissions in
+   the library folder"
+  [ ]
+  (let [ source (:source config) ]
+    (-> source file .canWrite)))
